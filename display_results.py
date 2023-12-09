885d4ad25bed56232a94b44d463f0412c3bf2611
nikonufrienko <nikonufrienko@gmail.com>
2023-12-09 03:14:56 +0300

    Fixed text in ui

--- display_results.py
+++ display_results.py
@@ -3,7 +3,7 @@ import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel
 from PyQt5.QtWidgets import QPushButton, QSizePolicy, QTextEdit, QHBoxLayout
 from PyQt5.QtWidgets import QSplitter
-from PyQt5.QtGui import QIcon, QPalette
+from PyQt5.QtGui import QIcon, QPalette, QTextCharFormat, QColor
 from PyQt5.QtCore import QSize, Qt
 import html
 
@@ -15,8 +15,6 @@ def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--input", default=None)
     parser.add_argument("-c", "--corrected", default=None)
-    parser.add_argument("-x", "--xpos", default=0)
-    parser.add_argument("-y", "--ypos", default=0)
     return parser.parse_args()
 
 
@@ -99,36 +97,37 @@ class DisplayResult(QWidget):
         original, modified = get_colored(self.original_str, self.modified_str)
         self.set_colored_text(self.te_input, original)
         self.set_colored_text(self.te_corrected, modified)
+        left_area = QWidget()
+        left_layout = QVBoxLayout()
+        left_area.setLayout(left_layout)
         right_area = QWidget()
         right_layout = QVBoxLayout()
-        self.te_input.setStyleSheet('QTextEdit { white-space: break-spaces; }')
-        self.te_corrected.setStyleSheet('QTextEdit { white-space: break-spaces; }')
         right_area.setLayout(right_layout)
-
+        left_layout.addWidget(self.te_input)
         right_layout.addWidget(self.te_corrected)
         right_layout.addWidget(CopyButton(modified_str=self.modified_str))
         splitter = QSplitter(Qt.Horizontal)
-        splitter.addWidget(self.te_input)
+        splitter.addWidget(left_area)
         splitter.addWidget(ArrowLabel())
         splitter.addWidget(right_area)
         splitter.setStretchFactor(0, 1)
         layout.addWidget(splitter)
         self.setLayout(layout)
         self.setWindowTitle("Spell checker")
-
+        splitter.setStretchFactor(0, 0)
+        splitter.setStretchFactor(3, 1)
         self.setGeometry(100, 100, 600, 200)
 
     def set_colored_text(self, text_edit, colored_str):
-        cursor = text_edit.textCursor()
+        char_format = QTextCharFormat()
         for char in colored_str:
             color = char[1]
-            escaped = "&nbsp;" if char[0] == ' ' else html.escape(char[0])
             if color:
-                cursor.insertHtml(
-                    f'<font style="color: white; background-color: {color}">{escaped}</font>'
-                )
+                char_format.setBackground(QColor(color))
             else:
-                cursor.insertHtml(f'<font>{escaped}</font>')
+                char_format.clearBackground()
+            text_edit.setCurrentCharFormat(char_format)
+            text_edit.insertPlainText(char[0])
 
 
 if __name__ == "__main__":
